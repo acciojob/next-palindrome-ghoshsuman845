5787e9f59cbb8f43bc0744e4c6925dec91c05bcb
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-11 04:51:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function nextPalindrome(num) {
-  //your JS code here.
-}
+  function isPalindrome(n) {
+    return n.toString() === n.toString().split("").reverse().join("");
+  }
 
+  while (!isPalindrome(++num)) {}
+  return num;
+}
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+alert(nextPalindrome(input)); // 22
